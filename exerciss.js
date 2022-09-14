0aaa0259c6fcb8da1ebe4b086d1e579818ab8e40
VinniC77 <vinniclunc@hotmail.com>
2022-09-14 00:08:52 -0300

    Almost Done

---
+++ exerciss.js
@@ -0,0 +1,49 @@
+// // let box = document.getElementById('box');
+
+// // box.addEventListener('click', () => {
+// //     console.log("I want to opne the box!");
+// // })
+
+// // Which variables below should be changed from let to const?
+
+// // The customer wants to order some stuff. Here are the details:
+// const basePrice = 520
+// const discount = 120
+// let shippingCost = 12
+// let shippingTime = "5-12 days"
+
+// // Whops! Turns out the shipping will be a bit more complex
+// shippingCost = 15
+// shippingTime = "7-14 days"
+
+// // Calculating the full price
+// const fullPrice = basePrice - discount + shippingCost
+
+// // Finally, notifying the customer
+// console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime)
+
+// let container = document.getElementById('container');
+// container.innerHTML = "<button onclick='buy()'>Buy!</button>";
+
+// function buy() {
+//     container.innerHTML += "<p>Thank you for buying</p>";
+// }
+
+// const recipient = "James";
+// const sender = "Vinicius";
+
+// const email = `Hey ${recipient}! How is it going? Cheers ${sender}`;
+
+// console.log(email);
+
+
+
+// THE FALSY VALUES IN JAVASCRIPT
+
+// false
+// 0
+// ""
+// null
+// undefined
+// NaN
+
--- index.js
+++ index.js
@@ -1,3 +1,87 @@
-let inputBtn = document.getElementById('input-btn');
+let myLeads = [];
 
-inputBtn.addEventListener('click', () => console.log('Button clicked!'));
+// 1. Turn the myLeads string into an array
+// myLeads = JSON.parse(myLeads);
+
+// // 2. Push a new value to the array
+// myLeads.push('www.google.com');
+
+// // 3. Turn the array into a string again
+// myLeads = JSON.stringify(myLeads);
+
+// // 4. Console.log the string using typeof to verify that it's a string
+// console.log(typeof myLeads);
+
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+
+// localStorage.setItem('google site', 'google.com') // to create an item
+// localStorage.getItem('google site') // to bring the value - google.com
+// localStorage.clear(); // to clear the localStorage
+
+localStorage.clear();
+// Get the leads from the localStorage
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem(myLeads));
+
+if(leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  render(myLeads);
+}
+
+
+const tabs = [
+  {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener('click', () => {
+  myLeads.push(tabs[0].url)
+  localStorage.setItem(myLeads, JSON.stringify(myLeads))
+  render(myLeads);
+})
+
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+    // With the above code we are doing 3 things:
+    // - Creating an html tag (<li>)
+    // - Setting a text content with myLeads[i]
+    // - Appending (anexando) something to a variable with ulEl.innerHTML
+    // - So, there is another way to write the code to have the same result:
+    // const li = document.createElement('li');
+    // li.textContent = myLeads[i];
+    // ulEl.append(li);
+    // A more performed way to write it would be like this:
+    // listItems += "<li><a href=" + myLeads[i] + " target='_blank'>" + myLeads[i] + "</a></li>";
+    listItems += `<li>
+                    <a href="${leads[i]}" target="_blank">
+                        ${leads[i]}
+                    </a>
+                  </li>
+                `;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+deleteBtn.addEventListener('dblclick', () => {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+})
+
+
+// Store it in a variable, leadsFromLocalStorage
+// Log out the variable
+
+inputBtn.addEventListener("click", () => {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  // Save the myLeads array to localStorage 
+    // PS: remember JSON.stringify()
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads);
+});
